773c89228395c83a3768340fab89ca82726fe34c
Elazar <elazarbsh@gmail.com>
2023-11-07 19:03:21 +0200

    remove unneeded files

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { Vec3 } from "./vec3.js";
+export { Model } from "./model.js";
+export { Camera } from "./camera.js";
+export { Light } from "./light.js";
+export { Renderer } from "./web-renderer.js";
+export { Scene } from "./scene.js";
+export { BrowserFreeCameraControls } from "./browser-free-camera-controls.js";
+export { BrowserMouseControls } from "./browser-mouse-controls.js";
+export { RGBA } from "./rgba.js";
+export { Geometry } from "./geometry.js";
+export { Texture } from "./texture.js";
+export { Material } from "./material.js";
+export { TextureLoader } from "./texture-loader.js";
+export { ModelLoader } from "./model-loader.js";
