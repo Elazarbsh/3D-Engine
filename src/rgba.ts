4dd5de7ed04454c0b343e0bbfa6b0a022176fa41
Elazar <elazarbsh@gmail.com>
2024-01-27 14:18:16 +0200

    add hex string to rgba conversions

--- src/rgba.ts
+++ src/rgba.ts
@@ -13,6 +13,38 @@ export class RGBA{
         this._alpha = alpha;
     }
 
+    public static fromHex(hex: string): RGBA {
+        // Check if the hex string is a valid length and contains only valid characters
+        const validHexRegex = /^#?[0-9a-fA-F]{6}$/;
+        if (!validHexRegex.test(hex)) {
+            throw new Error(`Invalid hex color string. string=${hex}`);
+        }
+    
+        // Remove the optional leading #
+        hex = hex.replace(/^#/, '');
+    
+        const bigint = parseInt(hex, 16);
+    
+        const red = (bigint >> 16) & 255;
+        const green = (bigint >> 8) & 255;
+        const blue = bigint & 255;
+    
+        return new RGBA(red, green, blue);
+    }
+    
+      // Function to get the color as a hex string
+    public toHex(): string {
+        const redHex = this.toHexComponent(this.red);
+        const greenHex = this.toHexComponent(this.green);
+        const blueHex = this.toHexComponent(this.blue);
+        return `#${redHex}${greenHex}${blueHex}`;
+    }
+
+    private toHexComponent(value : number) : string{
+        const hex = value.toString(16);
+        return hex.length === 1 ? '0' + hex : hex;
+    }
+
     public get red() {
         return this._red;
     }
