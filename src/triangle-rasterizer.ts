07b56018cb7d36c9a597b5766b9c3708ff8f06dc
Elazar <elazarbsh@gmail.com>
2024-05-03 17:16:06 +0300

    inflate triangles to prevents gaps

--- src/triangle-rasterizer.ts
+++ src/triangle-rasterizer.ts
@@ -13,7 +13,7 @@ export class TriangleRasterizer {
         this._depthBuffer = depthBuffer;
     }
 
-    private drawPixel(canvas : HTMLCanvasElement,x: number, y: number, color: RGBA, alpha: number) {
+    private drawPixel(canvas: HTMLCanvasElement, x: number, y: number, color: RGBA, alpha: number) {
         var roundedX = Math.round(x);
         var roundedY = Math.round(y);
         var index = 4 * (canvas.width * roundedY + roundedX);
@@ -23,11 +23,34 @@ export class TriangleRasterizer {
         this._imageData.data[index + 3] = alpha;
     }
 
-    public fillTriangle(canvas : HTMLCanvasElement, tri : Tri, material : Material) {
+    public fillTriangle(canvas: HTMLCanvasElement, tri: Tri, material: Material) {
         const ctx = canvas.getContext("2d");
 
         if (ctx === null)
             throw new Error("an error occured while drawing");
+
+        ctx.imageSmoothingEnabled = false;
+
+        let v1 = tri.v1;
+        let v2 = tri.v2;
+        let v3 = tri.v3;
+
+        let xc = (v1.x + v2.x + v3.x) / 3;
+        let yc = (v1.y + v2.y + v3.y) / 3;
+
+        // Calculate the distance between centroid and each vertex
+        let dist1 = Math.sqrt((xc - v1.x) ** 2 + (yc - v1.y) ** 2);
+        let dist2 = Math.sqrt((xc - v2.x) ** 2 + (yc - v2.y) ** 2);
+        let dist3 = Math.sqrt((xc - v3.x) ** 2 + (yc - v3.y) ** 2);
+
+        // Inflate each vertex by 0.5 pixels
+        let inflationFactor = 0.5 / Math.max(dist1, dist2, dist3);
+        let x1 = xc + (v1.x - xc) * (1 + inflationFactor);
+        let x2 = xc + (v2.x - xc) * (1 + inflationFactor);
+        let x3 = xc + (v3.x - xc) * (1 + inflationFactor);
+        let y1 = yc + (v1.y - yc) * (1 + inflationFactor);
+        let y2 = yc + (v2.y - yc) * (1 + inflationFactor);
+        let y3 = yc + (v3.y - yc) * (1 + inflationFactor);
 
         if (material.wireframe == true) {
             ctx.strokeStyle = `rgb(${material.wireframeColor.red}, ${material.wireframeColor.green}, ${material.wireframeColor.blue})`;
@@ -38,17 +61,19 @@ export class TriangleRasterizer {
         ctx.fillStyle = `rgb(${material.color.red * fillIntensity}, ${material.color.green * fillIntensity}, ${material.color.blue * fillIntensity})`;
 
         ctx.beginPath();
-        ctx.moveTo(tri.v1.x, tri.v1.y);
-        ctx.lineTo(tri.v2.x, tri.v2.y);
-        ctx.lineTo(tri.v3.x, tri.v3.y);
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.lineTo(x3, y3);
         ctx.closePath();
+
         if (material.wireframe) {
             ctx.stroke();
         }
         ctx.fill();
     }
 
-    public textureTriangle(canvas : HTMLCanvasElement, tri: Tri, tex: Texture, materialColor: RGBA) {
+
+    public textureTriangle(canvas: HTMLCanvasElement, tri: Tri, tex: Texture, materialColor: RGBA) {
         let x1: number = tri.v1.x;
         let x2: number = tri.v2.x;
         let x3: number = tri.v3.x;
@@ -225,6 +250,142 @@ export class TriangleRasterizer {
 
                     if (tex_w > this._depthBuffer[index]) {
                         this.drawPixel(canvas, j, i, new RGBA(red, green, blue), color.alpha);
+                        this._depthBuffer[i * canvas.width + j] = tex_w;
+                    }
+                    t += tstep;
+                }
+            }
+        }
+    }
+
+
+    public rasterizeTriangle(canvas: HTMLCanvasElement, tri: Tri, materialColor: RGBA) {
+        let x1: number = tri.v1.x;
+        let x2: number = tri.v2.x;
+        let x3: number = tri.v3.x;
+
+        let y1: number = tri.v1.y;
+        let y2: number = tri.v2.y;
+        let y3: number = tri.v3.y;
+
+        let w1: number = tri.v1.w;
+        let w2: number = tri.v2.w;
+        let w3: number = tri.v3.w;
+
+        if (y2 < y1) {
+            [y1, y2] = [y2, y1];
+            [x1, x2] = [x2, x1];
+            [w1, w2] = [w2, w1];
+        }
+
+        if (y3 < y1) {
+            [y1, y3] = [y3, y1];
+            [x1, x3] = [x3, x1];
+            [w1, w3] = [w3, w1];
+        }
+
+        if (y3 < y2) {
+            [y2, y3] = [y3, y2];
+            [x2, x3] = [x3, x2];
+            [w2, w3] = [w3, w2];
+        }
+
+        let dy1 = y2 - y1;
+        let dx1 = x2 - x1;
+        let dw1 = w2 - w1;
+
+        let dy2 = y3 - y1;
+        let dx2 = x3 - x1;
+        let dw2 = w3 - w1;
+
+        let tex_w;
+
+        let dax_step = 0, dbx_step = 0,
+            du1_step = 0, dv1_step = 0,
+            dw1_step = 0, dw2_step = 0;
+
+        if (dy1) dax_step = dx1 / Math.abs(dy1);
+        if (dy2) dbx_step = dx2 / Math.abs(dy2);
+
+        if (dy1) dw1_step = dw1 / Math.abs(dy1);
+
+        if (dy2) dw2_step = dw2 / Math.abs(dy2);
+
+        if (dy1) {
+            for (let i = y1; i <= y2; i++) {
+                let ax = x1 + (i - y1) * dax_step;
+                let bx = x1 + (i - y1) * dbx_step;
+
+                let tex_sw = w1 + (i - y1) * dw1_step;
+                let tex_ew = w1 + (i - y1) * dw2_step;
+
+                if (ax > bx) {
+                    [ax, bx] = [bx, ax];
+                    [tex_sw, tex_ew] = [tex_ew, tex_sw];
+                }
+
+                tex_w = tex_sw;
+
+                const tstep = 1.0 / (bx - ax);
+                let t = 0.0;
+
+                for (let j = ax; j < bx; j++) {
+                    tex_w = (1.0 - t) * tex_sw + t * tex_ew;
+                    var index = Math.floor(i * canvas.width + j);
+
+                    if (tex_w > this._depthBuffer[index]) {
+                        const red: number = (materialColor.red) * tri.surfaceLightIntensity;
+                        const green: number = (materialColor.green) * tri.surfaceLightIntensity;
+                        const blue: number = (materialColor.blue) * tri.surfaceLightIntensity;
+
+                        this.drawPixel(canvas, j, i, new RGBA(red, green, blue), materialColor.alpha);
+                        this._depthBuffer[i * canvas.width + j] = tex_w;
+                    }
+                    t += tstep;
+                }
+            }
+        }
+
+        dy1 = y3 - y2;
+        dx1 = x3 - x2;
+        dw1 = w3 - w2;
+
+        if (dy1) dax_step = dx1 / Math.abs(dy1);
+        if (dy2) dbx_step = dx2 / Math.abs(dy2);
+
+        du1_step = 0, dv1_step = 0;
+        if (dy1) dw1_step = dw1 / Math.abs(dy1);
+
+        if (dy1) {
+            for (let i = y2; i <= y3; i++) {
+                let ax = x2 + (i - y2) * dax_step;
+                let bx = x1 + (i - y1) * dbx_step;
+
+                let tex_sw = w2 + (i - y2) * dw1_step;
+
+                let tex_ew = w1 + (i - y1) * dw2_step;
+
+                if (ax > bx) {
+                    [ax, bx] = [bx, ax];
+                    [tex_sw, tex_ew] = [tex_ew, tex_sw];
+                }
+
+                tex_w = tex_sw;
+
+                const tstep = 1.0 / (bx - ax);
+                let t = 0.0;
+
+                for (let j = ax; j < bx; j++) {
+                    tex_w = (1.0 - t) * tex_sw + t * tex_ew;
+
+                    const red: number = (materialColor.red) * tri.surfaceLightIntensity;
+                    const green: number = (materialColor.green) * tri.surfaceLightIntensity;
+                    const blue: number = (materialColor.blue) * tri.surfaceLightIntensity;
+
+                    var index = Math.floor(i * canvas.width + j);
+
+                    if (tex_w > this._depthBuffer[index]) {
+                        this.drawPixel(canvas, j, i, new RGBA(red, green, blue), materialColor.alpha);
                         this._depthBuffer[i * canvas.width + j] = tex_w;
                     }
                     t += tstep;
